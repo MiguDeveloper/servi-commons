f3b0db574fb67fd809fb2ead391756f2b9b7286b
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-11-03 21:45:23 -0500

    servicio tipo libreria eliminamos autoconfig database

--- src/main/java/pe/tuna/servicommons/ServiCommonsApplication.java
+++ src/main/java/pe/tuna/servicommons/ServiCommonsApplication.java
@@ -1,9 +1,9 @@
 package pe.tuna.servicommons;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ServiCommonsApplication {
-
-
 }
